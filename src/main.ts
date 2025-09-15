7e774af1490c317d6e5dde786c7b20d76a66ec95
Jonathan Jaber <jonjjaber@gmail.com>
2025-09-15 14:55:44 -0700

    Updated the starter code to include a basic event listener.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 const element = document.getElementById("characters");
 
-for (const element of elements){
+// for (const element of elements){
     
-    element.style.backgroundColor = "#CCC";
-}
+//     element.style.backgroundColor = "#CCC";
+// }
+
+const itemInput = document.querySelector<HTMLInputElement>("itemInput");
+const addButton = document.querySelector("#addButton");
+const list = document.querySelector("#list");
+
+addButton.addEventListener("click", () => {
+
+    const listItem = document.createElement("li");
+    listItem.textContent = itemInput.value;
+    list?.append(listItem);
+});
