e314caa66dcbef328a5c9c2ba289edc455172a80
Jonathan Jaber <jonjjaber@gmail.com>
2025-09-16 16:33:54 -0700

    Turned off strict mode in tsconfig. Also trying to make some buttons for this typescript website.

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,30 @@ document.addEventListener("DOMContentLoaded", () => {
     // color all the characters <p> items
 })
 
-for (const element of elements){
-    
-    element.style.backgroundColor = "#CCC";
-}
+const output = document.querySelector("#output");
+const charButton = document.querySelector("#charButton");
+const publisherButton = document.querySelector("#publisherButton");
 
-const itemInput = document.querySelector<HTMLInputElement>("itemInput");
-const addButton = document.querySelector("#addButton");
-const list = document.querySelector("#list");
-
-addButton.addEventListener("click", () => {
-
-    const listItem = document.createElement("li");
-    listItem.textContent = itemInput.value;
-    list?.append(listItem);
+charButton.addEventListener("click", () => {
+    output.innerHTML = "<div><p>Mario</p>" + "<p>Sonic</p>" + "<p>PacMan</p></div>";
 });
+
+publisherButton.addEventListener("click", () => {
+    output.innerHTML = "<div><p>Nintendo</p>" + "<p>Sega</p>" + "<p>Namco</p></div>";
+});
+
+// for (const element of elements){
+    
+//     element.style.backgroundColor = "#CCC";
+// }
+
+// const itemInput = document.querySelector<HTMLInputElement>("itemInput");
+// const addButton = document.querySelector("#addButton");
+// const list = document.querySelector("#list");
+
+// addButton.addEventListener("click", () => {
+
+//     const listItem = document.createElement("li");
+//     listItem.textContent = itemInput.value;
+//     list?.append(listItem);
+// });
